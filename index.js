cd3bb6bd1a2847ef1ff20f5522f7b27a4d4cde2b
asifmahmud00 <asif.mahmud00@gmail.com>
2023-09-18 00:09:42 +0600

    23

--- index.js
+++ index.js
@@ -34,8 +34,21 @@ async function run() {
     const cartCollection = client.db("bistroDb").collection("carts");
 
     //User related apis
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+
     app.post('/users', async(req,res) => {
       const user = req.body;     
+      console.log(user);
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(query);
+      console.log('existingUser',existingUser);
+      if(existingUser){
+        return res.send({message:'Usr already exists'})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
